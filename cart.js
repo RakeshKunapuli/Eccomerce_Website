2fd3134d178da6dd7911e868f66368a6f38442ad
Sai Rakesh Kunapuli <sairakesh2494@gmail.com>
2023-12-02 12:25:30 +0530

    third Commit

--- cart.js
+++ cart.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const cartIcon = document.getElementById("cartIcon");
   cartItems = JSON.parse(localStorage.getItem("cart")) || [];
   var cartMain = document.querySelector(".cart-item-wrapper");
+  var placeOrderDiv = document.querySelector('.placeOrderDiv')
 
   updateCount();
   cartIcon.addEventListener("click", function () {
@@ -21,7 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
   cartItems.forEach((cart, i) => {
     var cartDiv = document.createElement("div");
     cartDiv.classList.add(`cartItems`);
-    cartDiv.setAttribute("onclick", `forwordTo(${cart.id})`);
+    // cartDiv.setAttribute("onclick", `forwordTo(${cart.id})`);
     cartDiv.innerHTML += `<div class="cartImage" >
       <img style="width: 100%;" src="${cart.image}" alt="${cart.title}">
     </div>
@@ -29,11 +30,39 @@ document.addEventListener("DOMContentLoaded", function () {
       <p><b>Title:</b>${cart.title}</p>
       <p><b>Description:</b>${cart.description}</p>
       <p><b>Price:<sup>$</sup></b>${cart.price}</p>
+      <button class='viewItembtn' onclick='forwordTo("${cart.id}")'>View Item</button>
       <button class='removebtn' onclick='removeItem("${cart.id}")'>Remove Item</button>
     </div>`;
     cartMain.appendChild(cartDiv);
   });
+  var placeOrder = document.createElement("button");
+    placeOrder.classList.add('placeorderbtn');
+    placeOrder.innerText='Place Order'
+
+  if(cartItems.length === 0){
+    placeOrder.style.display='none'
+    cartMain.innerHTML+= '<h1>CART IS EMPTY ADD ITEMS TO PLACE ORDER  </h1>'
+}else{
+    placeOrder.addEventListener('click',function(){
+      // var cartMain = document.querySelector(".cart-item-wrapper");
+      var cartDiv = document.querySelectorAll('.cartItems');
+      cartDiv.forEach(item => {
+        item.style.display='none';
+      });
+        placeOrderDiv.style.display='block'
+        localStorage.setItem("cart", JSON.stringify([]));
+        setTimeout(function(){
+          window.location.href='./index.html'
+        },5000)
+        placeOrder.innerText='Order Placed'
+    })
+    cartMain.appendChild(placeOrder);
+}
 });
+
+
+
+
 
 function removeItem(id) {
   var index = cartItems.findIndex((item) => item.id === id);
@@ -58,7 +87,7 @@ function renderCart() {
   cartItems.forEach((cart, i) => {
     var cartDiv = document.createElement("div");
     cartDiv.classList.add(`cartItems`);
-    cartDiv.setAttribute("onclick", `forwordTo(${cart.id})`);
+    // cartDiv.setAttribute("onclick", `forwordTo(${cart.id})`);
     cartDiv.innerHTML += `<div class="cartImage" >
       <img style="width: 100%;" src="${cart.image}" alt="${cart.title}">
     </div>
@@ -66,12 +95,38 @@ function renderCart() {
       <p><b>Title:</b>${cart.title}</p>
       <p><b>Description:</b>${cart.description}</p>
       <p><b>Price:<sup>$</sup></b>${cart.price}</p>
+      <button class='viewItembtn' onclick='forwordTo("${cart.id}")'>View Item</button>
       <button class='removebtn' onclick='removeItem("${cart.id}")'>Remove Item</button>
     </div>`;
     cartMain.appendChild(cartDiv);
+   
   });
+if(cartItems.length === 0){
+    placeOrder.style.display='none'
+    cartMain.innerHTML+= '<h1>CART IS EMPTY ADD ITEMS TO PLACE ORDER  </h1>'
+}else{
+    var placeOrder = document.createElement("button");
+    placeOrder.classList.add('placeorderbtn');
+    placeOrder.innerText='Place Order'
+    placeOrder.addEventListener('click',function(){
+      // var cartMain = document.querySelector(".cart-item-wrapper");
+      var cartDiv = document.querySelectorAll('.cartItems');
+      cartDiv.forEach(item => {
+        item.style.display='none';
+      });
+        placeOrderDiv.style.display='block'
+        localStorage.setItem("cart", JSON.stringify([]));
+        setTimeout(function(){
+          window.location.href='./index.html'
+        },5000)
+        placeOrder.innerText='Order Placed'
+    })
+    cartMain.appendChild(placeOrder);
+}
 }
 
 function forwordTo(id) {
   window.location.href = `productspec.html?item=${id}`;
 }
+
+
